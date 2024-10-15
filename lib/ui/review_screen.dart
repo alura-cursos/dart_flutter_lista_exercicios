7067668ad5e0bd7846b97ffe5abe07a8e206c035
ricarthlima <rrsl@cin.ufpe.br>
2024-10-15 14:59:51 -0300

    lista de exercícios da aula 01 (corrigido)

--- lib/ui/review_screen.dart
+++ lib/ui/review_screen.dart
@@ -9,31 +9,26 @@ class ReviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "${review.title} (${review.year})",
-              style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
+      body: Column(
+        children: [
+          Text(
+            "${review.title} (${review.year})",
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
             ),
-            Row(
-              children: List.generate(
-                review.stars,
-                (index) {
-                  return const Text("⭐");
-                },
-              ),
+          ),
+          Row(
+            children: List.generate(
+              review.stars,
+              (index) {
+                return const Text("⭐");
+              },
             ),
-            Text("Data: ${review.date.toString().substring(0, 10)}"),
-            Text(review.review),
-          ],
-        ),
+          ),
+          Text("Data: ${review.date.toString().substring(0, 10)}"),
+          Text(review.review),
+        ],
       ),
     );
   }
