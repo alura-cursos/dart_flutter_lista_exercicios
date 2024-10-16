77ed008bc66f1e29c2813ac8d0700ca61be1b982
ricarthlima <rrsl@cin.ufpe.br>
2024-10-16 12:55:31 -0300

    lista de exercícios da aula 02 (corrigido)

--- lib/ui/curriculum_screen.dart
+++ lib/ui/curriculum_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../models/curriculum.dart';
 import '../models/experience.dart';
-import '../models/project.dart';
 
 class CurriculumScreen extends StatelessWidget {
   final Curriculum curriculum;
@@ -13,118 +12,92 @@ class CurriculumScreen extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(32.0),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset(
-                "assets/foto.jpg",
-                height: 128,
-                width: 128,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(
+              "assets/foto.jpg",
+              height: 128,
+              width: 128,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              curriculum.name,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 8),
-              Text(
-                curriculum.name,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              "🎓 Educação",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 16),
-              const Text(
-                "🎓 Educação",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: List.generate(
+                curriculum.listEducation.length,
+                (index) {
+                  Experience experience = curriculum.listEducation[index];
+                  return Text(
+                      "* ${experience.title} (${experience.organization} - ${experience.dateStart.year})");
+                },
               ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: List.generate(
-                  curriculum.listEducation.length,
-                  (index) {
-                    Experience experience = curriculum.listEducation[index];
-                    return Text(
-                        "* ${experience.title} (${experience.organization} - ${experience.dateStart.year})");
-                  },
-                ),
+            ),
+            const SizedBox(height: 32),
+            const Text(
+              "🧠 Experiência profissional",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 32),
-              const Text(
-                "🧠 Experiência profissional",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: List.generate(
-                  curriculum.listProfessional.length,
-                  (index) {
-                    Experience experience = curriculum.listProfessional[index];
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          experience.title,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(experience.organization),
-                        (experience.obs != null)
-                            ? Text("(${experience.obs!})")
-                            : const SizedBox(),
-                        (experience.dateEnd != null)
-                            ? Text(
-                                "${experience.dateStart.year} - ${experience.dateEnd!.year}")
-                            : Text("${experience.dateStart.year} - atual"),
-                        const SizedBox(height: 8),
-                      ],
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(height: 32),
-              const Text(
-                "📲 Projetos",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Column(
-                children:
-                    List.generate(curriculum.listProjects.length, (index) {
-                  Project project = curriculum.listProjects[index];
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: List.generate(
+                curriculum.listProfessional.length,
+                (index) {
+                  Experience experience = curriculum.listProfessional[index];
                   return Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      const SizedBox(height: 16),
-                      Image.asset(project.imagePath),
                       Text(
-                        project.title,
-                        textAlign: TextAlign.center,
+                        experience.title,
                         style: const TextStyle(
-                          fontSize: 18,
                           fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Text(
-                        project.description,
-                        textAlign: TextAlign.center,
-                      ),
+                      Text(experience.organization),
+                      (experience.obs != null)
+                          ? Text("(${experience.obs!})")
+                          : const SizedBox(),
+                      (experience.dateEnd != null)
+                          ? Text(
+                              "${experience.dateStart.year} - ${experience.dateEnd!.year}")
+                          : Text("${experience.dateStart.year} - atual"),
+                      const SizedBox(height: 8),
                     ],
                   );
-                }),
-              )
-            ],
-          ),
+                },
+              ),
+            ),
+            const SizedBox(height: 32),
+            const Text(
+              "📲 Projetos",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
         ),
       ),
     );
